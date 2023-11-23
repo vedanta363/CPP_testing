4d98a0fd8421c426977cae04d73b331abbbfc359
Vedant Agrawal <84488357+vedanta363@users.noreply.github.com>
2023-11-23 20:03:34 +0545

    Create print_num.cpp

---
+++ 1/print_num.cpp
@@ -0,0 +1,13 @@
+// Print Number Entered by User
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
